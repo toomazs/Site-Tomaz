11a58219bab527835a85e5eb3e1a7deea34d9bc8
Eduardo Tomaz <eduardotoomaz@outlook.com>
2024-08-20 13:12:27 -0300

    V4

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 const iconBoxes = document.querySelectorAll(".icon-box");
 const iconBoxContainers = document.querySelectorAll(".icon-container");
-const closeBtns = document.querySelectorAll(".close-btn");
 const maximizeBtns = document.querySelectorAll(".maximize-btn");
 const body = document.querySelector("body");
+const popup = document.querySelector('.popup');
+const closeBtns = document.querySelectorAll(".close-btn");
 
-iconBoxes.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    let modal = btn.getAttribute("data-modal");
-    document.getElementById(modal).style.display = "block";
-    body.classList.add("prevent-background-scroll");
-  });
-});
-
+// Fechar popup ao clicar no botão de fechar
 closeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     let modal = btn.closest(".popup");
-    modal.style.display = "none";
-    body.classList.remove("prevent-background-scroll");
-    iconBoxContainers.forEach((container) => {
-      container.style.display = "flex";
+    modal.classList.add("closing");
+    setTimeout(() => {
+      modal.style.display = "none";
+      body.classList.remove("prevent-background-scroll");
+      iconBoxContainers.forEach((container) => {
+        container.style.display = "flex";
+      });
+    }, 500); // tempo de animação em milissegundos
+  });
+});
+
+// Abrir popup ao clicar em um icon-box
+iconBoxes.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let modal = btn.getAttribute("data-modal");
+    let currentModal = document.getElementById(modal);
+    currentModal.style.display = "block";
+    currentModal.classList.remove("closing");
+    body.classList.add("prevent-background-scroll");
+
+    // Fechar outros popups que estejam abertos
+    document.querySelectorAll('.popup').forEach((otherModal) => {
+      if (otherModal !== currentModal) {
+        otherModal.classList.add('closing');
+        setTimeout(() => {
+          otherModal.style.display = 'none';
+        }, 500);
+      }
     });
   });
 });
 
+// Fechar popup ao clicar fora dele
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("popup")) {
-    e.target.style.display = "none";
-    body.classList.remove("prevent-background-scroll");
+    e.target.classList.add('closing');
+    setTimeout(() => {
+      e.target.style.display = "none";
+      body.classList.remove("prevent-background-scroll");
+    }, 500);
   }
 });
 
+// Maximizar/minimizar popup
 maximizeBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     let modal = btn.closest(".popup");
@@ -51,6 +74,7 @@ maximizeBtns.forEach((btn) => {
   });
 });
 
+// Swiper
 var swiper = new Swiper(".swiper", {
   preventClicks: true,
   noSwiping: true,
