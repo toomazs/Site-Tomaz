92b74a48c41d72260b3e29870238f6c272594631
Eduardo Tomaz <eduardotoomaz@outlook.com>
2024-08-29 02:19:12 -0300

    php v2

--- email.php
+++ email.php
@@ -1,34 +1,29 @@
-
 <?php
 
-if(isset($_POST['email']) && !empty($_POST['email'])){
-    $name = addslashes($_POST['name']);
-    $email = addslashes($_POST['email']);
-    $message = addslashes($_POST['message']);
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
     $to = "eduardotoomaz@Outlook.com";
     $subject = "Contato pelo site-portfolio";
 
-    $body = "Nome: ".$name. "\r\n".
-            "E-mail: ".$email. "\r\n".
-            "Mensagem: ".$message;
-    $header = "From:eduardotoomaz@Outlook.com"."\r\n".
-            "Reply-To: ".$email;."\r\n".
-            "X=Mailer:PHP/".phpversion();
+    $body = "Nome: " . $name . "\r\n" .
+            "E-mail: " . $email . "\r\n" .
+            "Mensagem: " . $message;
+    $header = "From: eduardotoomaz@Outlook.com" . "\r\n" .
+            "Reply-To: " . filter_var($email, FILTER_SANITIZE_EMAIL) . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
 
-    if(mail($to,$subject,$body,$header)){
-
-        echo("Email enviado com sucesso")
-
-    } else {
-
-        echo("Email nao foi enviado")
-        
+    try {
+        if (mail($to, $subject, $body, $header)) {
+            echo "Email enviado com sucesso";
+        } else {
+            echo "Email não foi enviado";
+        }
+    } catch (Exception $e) {
+        echo "Erro ao enviar email: " . $e->getMessage();
     }
-    
-} else {
-    echo("Email nao foi enviado, favor verificar se o formulario foi preenchido corretamente");
-
 }
 
 ?>
